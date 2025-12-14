a4b9318ddbaaf7f47ba5eb0515ce44143c1363c0
Dreamer356 <glukhovsky.v@gmail.com>
2025-12-14 16:39:19 +0200

    +

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,30 +1,35 @@
 import Link from 'next/link'
+import TagsMenu from '../TagsMenu/TagsMenu'
 import css from './Header.module.css'
 
 export default function Header() {
-  return (
-    <header className={css.header}>
-      {/* Logo / Home */}
-      <Link href="/" aria-label="Home" className={css.headerLink}>
-        NoteHub
-      </Link>
-
-      {/* Main navigation */}
-      <nav aria-label="Main Navigation">
-        <ul className={css.navigation}>
-          <li className={css.navigationItem}>
-            <Link href="/" className={css.navigationLink}>
-              Home
+    return (
+        <header className={css.header}>
+            <Link href="/" aria-label="Home" className={css.headerLink}>
+                NoteHub
             </Link>
-          </li>
 
-          <li className={css.navigationItem}>
-            <Link href="/notes" className={css.navigationLink}>
-              Notes
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+            <nav aria-label="Main Navigation">
+                <ul className={css.navigation}>
+                    <li className={css.navigationItem}>
+                        <Link href="/" className={css.navigationLink}>
+                            Home
+                        </Link>
+                    </li>
+
+                    {/* Обов'язковий пункт меню */}
+                    <li className={css.navigationItem}>
+                        <Link href="/notes/filter/all" className={css.navigationLink}>
+                            All Notes
+                        </Link>
+                    </li>
+
+                    {/* Ваш дропдаун або меню тегів */}
+                    <li className={css.navigationItem}>
+                        <TagsMenu />
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
